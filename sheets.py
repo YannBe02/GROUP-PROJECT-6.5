9b519ef11da238c7a72becaf261c0b7067c2580d
YannBe02 <162722611+YannBe02@users.noreply.github.com>
2024-04-22 15:29:23 +0200

    Add files via upload

---
+++ sheets.py
@@ -0,0 +1,15 @@
+import gspread 
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets" ,"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("Database").sheet1
+
+data = sheet.get_all_records()
+
+pprint(data)
