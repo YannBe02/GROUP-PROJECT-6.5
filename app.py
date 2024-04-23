dab2fe331e26d1808877fcf079ab880e61bb3e81
YannBe02 <162722611+YannBe02@users.noreply.github.com>
2024-04-23 17:47:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import sheets
 
-st.set_page_config(page_title="UNICLUBS", page_icon=":computer:")
+st.set_page_config(page_title="HSG UNICLUBS", page_icon=":computer:")
 
-st.title("WELCOME TO UNICLUBS")
-st.write("""
-We designed this website to help you find the best club corresponding to your criteria
+
+
+st.title('''
+         :red[WELCOME TO UNICLUBS]''')
+st.header("""
+We designed this website to help you finding the best club corresponding to your criteria
 """)
 
-st.subheader("Point of interests")
+
+st.subheader("Point of Interests")   
 modules = [
     "AV",
     "Networking and Think Tanks",
@@ -26,7 +30,7 @@ modules = [
 selected_modules = st.multiselect("Select the topics you're interested in:", modules)
 for module in selected_modules:
     st.write(f"📘 {module}")
-
+    
 
 st.subheader("Language(s)")
 languages = [
@@ -40,40 +44,70 @@ languages = [
     "Arabic",
 ]
 
-selected_languages = st.multiselect("Select the language(s) you wish to speak", languages)
+selected_languages = st.multiselect("Select the language(s) you wish to speak in the club", languages)
 for language in selected_languages:
     st.write(f"You've selected {language}")
 
-st.subheader("Other languages")
-other_language = st.text_input("If you did not find the desired languages, which ones do you wish?", placeholder="Type the language(s)...")
-if other_language != "":
-    st.write(f"You are looking for a club speaking {other_language}! We are looking forward to help you finiding your dreamed club!")
-
-st.subheader("How big should be the club?")
+st.subheader("Number of Members")
 number_of_members = st.radio(
     "Select the number of members",
-    ("1", "10", "50", "100", "200", "300+")
-) #could even use a slider
+    ("11-30", "31-50", "51-100", "101 & more")
+)
+st.write(f"You've selected {number_of_members} members in the club!")
 
-    
-if {number_of_members} == {"1"}:
-    st.write(f"You've selected {number_of_members} member in the club!")
-else:
-    st.write(f"You've selected {number_of_members} members in the club!") #why the if condition? for the "s" if the mf choose 1 member lol
+
+st.subheader("Accreditation")
+st.write("Do you wish to find a club who are giving credit for work?")
+credits = st.radio(
+    "Select:",
+    ("Yes", "No")
+)
+st.write(f"You've selected {credits}!")
+
+data = sheets.get_data_from_google_sheets(selected_modules, number_of_members, credits)
+
+
+
+def format_database_record(record):
+    st.markdown(f"**Name:** {record['NAME']}")
+    st.markdown(f"**Description:** {record['DESCRIPTION']}")
+    st.markdown(f"**Category:** {record['CATEGORY']}")
+    st.markdown(f"**Instagram:** [{record['INSTAGRAM']}]({record['INSTAGRAM']})")
+    st.markdown(f"**Facebook:** [{record['FACEBOOK']}]({record['FACEBOOK']})")
+    st.markdown(f"**LinkedIn:** [{record['LINKEDIN']}]({record['LINKEDIN']})")
+    st.markdown(f"**Website:** [{record['WEBSITE']}]({record['WEBSITE']})")
+    st.markdown(f"**Email:** {record['EMAIL']}")
+    st.markdown(f"**Is Recruiting:** {record['IS RECRUITING']}")
+    st.markdown(f"**Number of Members:** {record['NUMBER OF MEMBERS']}")
+    st.markdown(f"**Accreditation:** {record['ACCREDITATION']}")
+    st.markdown(f"**Point of Interests:** {record['POINT OF INTERESTS']}")
+    st.markdown(f"**Languages:** {record['LANGUAGE(S)']}")
+
+    st.markdown("---")  # Add a horizontal line between records
+
+# Assume 'data' is a list of records from the database for a specific category
+for record in data:
+    format_database_record(record)
+
+
+
+
+
+
 
 st.subheader("Who are we?")
-if st.button("Us"):
+if st.button("Team"):
     st.write("We are Group 6.5!")
     st.write("""
              We are a team of 5 students at the University of St. Gallen who decided to create a website to easily find a club when you don't know what to do out 
              out of the University!
-             Shania Metrailler, 
+             Shania Metrailler,
              Léa Hurther,
-             Frédéric De Buys,
+             Frédéric de Buys,
              Sascha Huber,
              Edouard Vouillamoz,
-             Yann Bernasconi             
-             
+             Yann Bernasconi
+            
              """)
     
 
