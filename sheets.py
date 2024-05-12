0c0de398909b3820b555a0cd7493d13f777485c0
YannBe02 <162722611+YannBe02@users.noreply.github.com>
2024-05-12 13:10:36 +0200

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,9 +1,8 @@
-import gspread 
-from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint
+import gspread #importation of gspread, which allows to link our database (in the creds.json document, which is linked to APIs) and our app.py document
+from oauth2client.service_account import ServiceAccountCredentials #quite the same as abovementioned
 
-def get_data_from_google_sheets(modules=None, number_of_members=None, credits=None, languages=None):
-
+def get_data_from_google_sheets(modules=None, number_of_members=None, credits=None, languages=None):    #implementation of a function, so that we can use it in the app.py document
+#it allows literaly to link the database with the selection of the user on the website
     scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets" ,"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
     creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
@@ -14,18 +13,18 @@ def get_data_from_google_sheets(modules=None, number_of_members=None, credits=No
 
     data = sheet.get_all_records()
 
-    filtered_data = data
+    filtered_data = data #allows to filter the datas of the database with the selection of the website user
 
-    if modules:
+    if modules: #filter for the point of interests
         filtered_data = [record for record in filtered_data if record.get("POINT OF INTERESTS") in modules]
 
-    if number_of_members:   
+    if number_of_members:   #filter for the number of memebers 
         filtered_data = [record for record in filtered_data if record.get("NUMBER OF MEMBERS") ==  number_of_members]
     
-    if credits:
+    if credits: #filter for the accreditation
         filtered_data = [record for record in filtered_data if record.get("ACCREDITATION") == credits]      
 
-    if languages:
+    if languages: #filter for the languages 
         filtered_data = [record for record in filtered_data if record.get("LANGUAGE(S)") in languages]
 
     return filtered_data
